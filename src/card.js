da637025e526ee6046c9c527cb94b9ea374f8230
Janek <s16503@pjwstk.edu.pl>
2021-11-21 14:57:08 +0100

    rotate animation in css

--- src/card.js
+++ src/card.js
@@ -8,5 +8,18 @@ export class Card{
         this.element.style.setProperty('--label-color',labelColor)
         this.element.style.setProperty('--color',color)
         this.element.textContent = color;
+        this.element.addEventListener('click', this.onClick.bind(this));
+        this.putFaceDown();
+    }
+
+    onClick(){
+       this.putFaceUp();
+    }
+
+    putFaceDown(){
+        this.element.classList.add('face-down')
+    }
+    putFaceUp(){
+        this.element.classList.remove('face-down')
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'src/index.scss';
 import {colors} from 'src/data'
-import {findElement, cloneTemplateContent} from 'src/utils';
+import {findElement} from 'src/utils';
 import { Card } from './card';
 import { Board } from './board';
 
@@ -11,29 +11,20 @@ const boardTemplate = findElement(document,'#board-template');
 function run() {
   console.log('Memory Game');
   const selectedColors = colors.slice(0, 12);
-  // console.log('colors', selectedColors)
-
-    // console.log('slotTemplate',slotTemplate)
-    // console.log('cardTemplate',cardTemplate)
-    // console.log('cardTemplate',boardTemplate)
-
+  
     const board = new Board({template:boardTemplate, slotTemplate})
-    // const boardElement = cloneTemplateContent(boardTemplate);
 
-    // console.log('slotElement', slotElement)
-    // console.log('cardElement', cardElement)
-    // console.log('boardElement', boardElement)
 
     selectedColors.forEach(({color, labelColor})=>{
       console.log('color', color)
       console.log('label', labelColor)
 
-      // const slotElement = cloneTemplateContent(slotTemplate);
+    
       const card = new Card({color, labelColor, template: cardTemplate})
-
-      // slotElement.appendChild(card.element)
-      // board.element.appendChild(slotElement)
       board.addCard(card)
+
+      const card2 = new Card({color, labelColor, template: cardTemplate})
+      board.addCard(card2)
     })
 
    document.body.prepend(board.element)
