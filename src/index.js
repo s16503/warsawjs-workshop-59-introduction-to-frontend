3b581f447df13d7af844151b49b0aaae7345178d
Janek <s16503@pjwstk.edu.pl>
2021-11-21 12:52:11 +0100

    find templates and copy them

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import 'src/index.scss';
+import {colors} from 'src/data'
+import {findElement, cloneTemplateContent} from 'src/utils';
 
 function run() {
   console.log('Memory Game');
-}
+  const selectedColors = colors.slice(0, 12);
+  console.log('colors', selectedColors)
+
+  const slotTemplate = findElement(document,'#slot-template');
+  const cardTemplate=findElement(document,'#card-template');
+    console.log('slotTemplate',slotTemplate)
+    console.log('cardTemplate',cardTemplate)
+    const slotElement = cloneTemplateContent(slotTemplate);
+    const cardElement = cloneTemplateContent(cardTemplate);
+    console.log('slotElement', slotElement)
+  }
 
 run();
